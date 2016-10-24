7b3eb2358e300ca0984f9aded5773556bb53ce42
Marina <mary.dvorskaya@yandex.ua>
2016-10-24 23:57:42 +0300

    second commit

---
+++ marina/src/main/java/Animals.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Марина on 24.10.2016.
+ */
+public class Animals {
+    String name;
+    String type;
+    int age;
+    int mass;
+    int height;
+}
--- marina/src/main/java/Test.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Evgeniy on 24.10.2016.
- */
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
